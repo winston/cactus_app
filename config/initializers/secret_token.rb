a88df6f027c201212406586ebe38b1916b950ab9
Winston <winston.yongwei@gmail.com>
2012-05-14 09:25:29 +0800

    Getting started.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CactusApp::Application.config.secret_token = '1099fc74d7dfaae6feb9c56b4552537c930dd04d0f57a718c315353bc068f55ca04bce59d7f01f341965ab86adca17f383c6ea45786ff194f940a1b70236f47e'
