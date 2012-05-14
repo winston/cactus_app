14499396ca8a3839bd17945b009ff19b0ab2519b
Winston <winston.yongwei@gmail.com>
2012-05-14 17:05:09 +0800

    Update cactus specs.

--- public/cactus_spec/application_spec.js
+++ public/cactus_spec/application_spec.js
@@ -2,29 +2,28 @@
 // .alert
 
 //// round box
-Cactus.expect(".alert", "margin" ).toEqual("10px 0px");
-Cactus.expect(".alert", "padding").toEqual("10px");
-Cactus.expect(".alert", "border-width").toEqual("1px");
-Cactus.expect(".alert", "border-radius").toEqual("10px");
+Cactus.expect(".alert").toHaveMargin("10px 0px");
+Cactus.expect(".alert").toHavePadding("10px");
+Cactus.expect(".alert").toHaveBorderWidth("1px");
 
 //// colors
-Cactus.expect(".alert", "border-color").toHaveColor("#FF0000");
+Cactus.expect(".alert").toHaveBorderColor("#FF0000");
 Cactus.expect(".alert", "background-color").toHaveColor("#FFCCCC");
 
 //// font
 Cactus.expect(".alert p", "font-size").toEqual("20px");
 
+
 //////////
 // .notice
 
 //// round box
-Cactus.expect(".notice", "margin" ).toEqual("10px 0px");
-Cactus.expect(".notice", "padding").toEqual("10px");
-Cactus.expect(".notice", "border-width").toEqual("1px");
-Cactus.expect(".notice", "border-radius").toEqual("10px");
+Cactus.expect(".notice").toHaveMargin("10px 0px");
+Cactus.expect(".notice").toHavePadding("10px");
+Cactus.expect(".notice").toHaveBorderWidth("1px");
 
 //// box colors
-Cactus.expect(".notice", "border-color").toHaveColor("#008000");
+Cactus.expect(".notice").toHaveBorderColor("#008000");
 Cactus.expect(".notice", "background-color").toHaveColor("#4DFF4D");
 
 //// font
@@ -43,10 +42,9 @@ Cactus.expect(".notice p", "font-size").toEqual("20px");
 //// Refactored
 //
 //function test_round_box(selector) {
-//  Cactus.expect(selector, "margin" ).toEqual("10px 0px");
-//  Cactus.expect(selector, "padding").toEqual("10px");
-//  Cactus.expect(selector, "border-width").toEqual("1px");
-//  Cactus.expect(selector, "border-radius").toEqual("10px");
+//  Cactus.expect(selector).toHaveMargin("10px 0px");
+//  Cactus.expect(selector).toHavePadding("10px");
+//  Cactus.expect(selector).toHaveBorderWidth("1px");
 //}
 //
 //function test_large_font(selector) {
@@ -55,11 +53,11 @@ Cactus.expect(".notice p", "font-size").toEqual("20px");
 //
 //test_round_box(".alert");
 //test_large_font(".alert p");
-//Cactus.expect(".alert", "border-color").toHaveColor("#FF0000");
+//Cactus.expect(".alert").toHaveBorderColor("#FF0000");
 //Cactus.expect(".alert", "background-color").toHaveColor("#FFCCCC");
 //
 //test_round_box(".notice");
 //test_large_font(".notice p");
-//Cactus.expect(".notice", "border-color").toHaveColor("#008000");
+//Cactus.expect(".notice").toHaveBorderColor("#008000");
 //Cactus.expect(".notice", "background-color").toHaveColor("#4DFF4D");
 
